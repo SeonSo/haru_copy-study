7e2e6aee8f7474bc56535c891fe1b3a89f6cae64
YoYag <la2043432@gmail.com>
2022-07-25 10:47:50 +0900

    시작

---
+++ src/main/java/com/project1/haruco/HarucoApplication.java
@@ -0,0 +1,13 @@
+package com.project1.haruco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarucoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarucoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project1/haruco/HarucoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.haruco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarucoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
